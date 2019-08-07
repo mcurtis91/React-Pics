3a27fd04faf230cc8182a7e0c49e4db994863615
mcurtis91 <mikecurtis1991@gmail.com>
2019-08-07 16:46:17 -0700

    Removed Key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID GetYourOwn"
+    Authorization:
+      "Client-ID 07da1b96cda3f63c11058227c77401136bc8da8e0a24c2bb7994cf0827604ad9"
   }
 });
